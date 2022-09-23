3b7bed51e83b1f2be3d574c2d9e8c9c7f2a90633
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-09-23 10:17:52 +0200

    Update and refactoring of classes
    Updated names in order for merge to be compatible.

--- src/main/java/MVC/view/MainMenu.java
+++ src/main/java/MVC/view/MainMenu.java
@@ -1,5 +1,5 @@
 
-package MVC.Menu;
+package MVC.view;
 
 import javafx.application.Application;
 import javafx.scene.layout.AnchorPane;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module MVC {
     requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-    exports MVC.Menu;
     exports MVC.controller;
-
-
+    exports MVC.view;
 }
