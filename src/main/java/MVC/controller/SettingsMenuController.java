40ec0aaef17c3f6608c9b6112a1a2e3537a0dd3b
Alva Johansson <alva.j.2002@gmail.com>
2022-09-23 10:08:38 +0200

    added to do's

--- src/main/java/MVC/controller/SettingsMenuController.java
+++ src/main/java/MVC/controller/SettingsMenuController.java
@@ -5,13 +5,14 @@ import javafx.event.ActionEvent;
 public class SettingsMenuController {
     public void backToMenu(ActionEvent e){
         System.out.println("menu");
+        //TODO make something happen
     }
     public void restartGame(ActionEvent e){
         System.out.println("restart");
-
+        //TODO make something happen
     }
     public void closeSettingsMenu(ActionEvent e){
         System.out.println("close");
-
+        //TODO make something happen
     }
 }
