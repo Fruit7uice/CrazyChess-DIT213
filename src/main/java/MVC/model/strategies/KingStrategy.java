d44a5f67d945f8fd4724351a32288c3c33f8abbd
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-09-28 14:59:19 +0200

    Methods for King and Queen
    Method-body for move - King & Queen

--- src/main/java/MVC/model/strategies/KingStrategy.java
+++ src/main/java/MVC/model/strategies/KingStrategy.java
@@ -1,4 +1,11 @@
 package MVC.model.strategies;
 
-public class KingStrategy {
+public class KingStrategy implements IMoveStrategy {
+
+
+    @Override
+    public void move(int startX, int startY, int endX, int end) {
+
+
+    }
 }
--- src/main/java/MVC/model/strategies/QueenStrategy.java
+++ src/main/java/MVC/model/strategies/QueenStrategy.java
@@ -1,9 +1,19 @@
 package MVC.model.strategies;
 
-public class QueenStrategy {
+public class QueenStrategy implements IMoveStrategy {
 
 
-    
+
+    public void move(int startX, int startY, int endX, int endY){
+
+
+
+
+
+    }
+
+
+
 
 
 
