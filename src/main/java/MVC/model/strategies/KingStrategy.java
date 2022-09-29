338aab7908679c631748750438be785f20e4fc78
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-09-29 08:05:33 +0200

    Strategies for King and Queen
    Both the methods regarding the different strategies for movies have been implemented.
    Question??
    - Should the behavior for castling also be here considering its a part of the behavior concerning move for the King??

--- src/main/java/MVC/model/strategies/KingStrategy.java
+++ src/main/java/MVC/model/strategies/KingStrategy.java
@@ -2,10 +2,30 @@ package MVC.model.strategies;
 
 public class KingStrategy implements IMoveStrategy {
 
+    // Question??
+    // Should the behavior for castling also be here considering
+    // it's a part of the behavior concerning move for the King??
+
+    /**
+     * Method for the strategy regarding the behavior for how the King may move
+     * @param startX Starting x-coordinate on the board
+     * @param startY Starting y-coordinate on the board
+     * @param endX Ending x-coordinate on the board
+     * @param endY Ending y-coordinate on the board
+     */
 
     @Override
-    public void move(int startX, int startY, int endX, int end) {
-
-
+    public void move(int startX, int startY, int endX, int endY) {
+        if (Math.abs(startX - endX) == 1 && (startY == endY)) {
+            startX = endX;
+        }
+        else if(Math.abs(startY - endY) == 1 && (startX == endX)){
+            startY = endY;
+            }
+        else if((Math.abs(startX - endX) == 1) && (Math.abs(startY - endY) == 1)) {
+            startX = endX;
+            startY = endY;
+        }
     }
 }
+
--- src/main/java/MVC/model/strategies/QueenStrategy.java
+++ src/main/java/MVC/model/strategies/QueenStrategy.java
@@ -3,19 +3,35 @@ package MVC.model.strategies;
 public class QueenStrategy implements IMoveStrategy {
 
 
+    /**
+     * Method for the strategy regarding the behavior for how the Queen may move
+     * @param startX Starting x-coordinate on the board
+     * @param startY Starting y-coordinate on the board
+     * @param endX Ending x-coordinate on the board
+     * @param endY Ending y-coordinate on the board
+     */
 
-    public void move(int startX, int startY, int endX, int endY){
 
-
-
-
-
+    public void move(int startX, int startY, int endX, int endY) {
+        if (Math.abs(startX - endX) == Math.abs(startY - endY)) { // Same behavior as for Bishop.
+            startX = endX;
+            startY = endY;
+        } else if (((Math.abs(startX - endX) > 0) && (startY == endY)) || // Same behavior as for Rook.
+                ((Math.abs(startY - endY) > 0) && (startX == endX))) {
+            startX = endX;
+            startY = endY;
+        }
     }
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
