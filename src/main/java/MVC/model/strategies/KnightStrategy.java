6b263ac0b410745b7580984319a3a09def5b49e4
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-09-30 19:15:30 +0200

    Update KnightStrategy.java
    Strategy for Knight has been implemented

--- src/main/java/MVC/model/strategies/KnightStrategy.java
+++ src/main/java/MVC/model/strategies/KnightStrategy.java
@@ -1,17 +1,27 @@
 package MVC.model.strategies;
 
 public class KnightStrategy {
+    public static void main(String[] args) {
+        KnightStrategy(0,0, 2, 2);
 
-    private int startX;
-    private int startY;
-    private int newX;
-    private int newY;
+    }
 
-    private int deltaKnightX = Math.abs(startX - newX);
-    private int deltaKnightY = Math.abs(startY - newY);
+    private static int startX;
+    private static int startY;
+    private static int newX;
+    private static int newY;
 
-    public boolean KnightStrategy(int startX, int startY, int newX, int newY){
-        if()
+    //private static int deltaKnightX = Math.abs(startX - newX);
+    //private static int deltaKnightY = Math.abs(startY - newY);
+
+    public static boolean KnightStrategy(int startX, int startY, int newX, int newY){
+        if(Math.abs(startX - newX) + Math.abs(startY - newY) == 3  &&
+                Math.abs(startX - newX) > 0 && Math.abs(startY - newY) > 0){
+            System.out.println("Valid move");
+            return true;
+        }else{System.out.println("Unvalid move");
+            return false;
+        }
 
     }
 
