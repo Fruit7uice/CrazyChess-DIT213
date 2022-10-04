f82b63300161e8c5daccf2e3f9f698240c871f7b
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-10-04 10:34:05 +0200

    Small fix strategies

--- src/main/java/MVC/model/strategies/PawnStrategy.java
+++ src/main/java/MVC/model/strategies/PawnStrategy.java
@@ -11,7 +11,7 @@ package MVC.model.strategies;
 public class PawnStrategy implements IMoveStrategy {
     private boolean hasMoved = false; // not finished, move logic to another class
 
-    @Override
+
     public boolean move(int startX, int startY, int endX, int endY) {
         if (!hasMoved) {          // not finished, move logic to another class.
             if ((endY - startY) == 2 || (endY - startY) == 1
