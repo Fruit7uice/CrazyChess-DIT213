c47b22641b61cabc7e7891f1c179ca517cdbc56f
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-10-04 10:35:39 +0200

    Small fix
    Deletion of redundant code

--- src/main/java/MVC/model/Pieces/Bishop.java
+++ src/main/java/MVC/model/Pieces/Bishop.java
@@ -5,32 +5,5 @@ public class Bishop extends Piece{
         super(xPos, yPos, width, height, firstImagePath, secondImagePath, type);
     }
 
-
-    @Override
-    public void move(){
-
-        /*
-        piece has been dragged
-
-        check: isOccupied
-        false -> move here;
-        true ->
-            case isOccupied of
-                tileType = getType.tile.desiredMove();
-
-                if this.type == tile.pieceType{
-                    then can't move here
-                    }
-                    else(this.type) == opposingColour{
-                        capture;
-                        diagonalStrategy; // kolla strategi
-                    }.
-
-         */
-
-
-
-
-
-    }
+    
 }
