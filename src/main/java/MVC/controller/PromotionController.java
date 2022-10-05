4fd1bfc3a884215ccffe78372a54ae1513cd4162
JOELDENFETE <90786954+JOELDENFETE@users.noreply.github.com>
2022-10-05 14:39:07 +0200

    Created PromotionController

---
+++ src/main/java/MVC/controller/PromotionController.java
@@ -0,0 +1,46 @@
+package MVC.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
+public class PromotionController {
+/*
+    @FXML
+    public Button bishopOption;
+
+    @FXML
+    public Button knightButton;
+
+    @FXML
+    public Button queenOption;
+
+    @FXML
+    public Button rookOption;
+
+ */
+    // vid varje knapp skall jag ta bort bonden och ersätta med den knappen man trycker pås
+
+    public void chooseBishop(ActionEvent event) throws IOException {
+        System.out.println("you choose bishop");
+
+    }
+
+    public void chooseKnight(ActionEvent event) throws IOException {
+        System.out.println("you choose Knight");
+
+    }
+
+    public void chooseQueen(ActionEvent event) throws IOException {
+        System.out.println("you choose Queen");
+
+    }
+
+    public void chooseRook(ActionEvent event) throws IOException{
+        System.out.println("you choose Rook");
+
+    }
+
+}
