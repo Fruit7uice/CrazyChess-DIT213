ee8596c979ef53d4a1bdcc20fb72734230adad56
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-10-06 08:09:36 +0200

    Update Castle.java
    Methods that check if the spaces between the king and the rook are free. Coordinates needs to be adjusted.

--- src/main/java/MVC/model/SpecialMoves/Castle.java
+++ src/main/java/MVC/model/SpecialMoves/Castle.java
@@ -1,10 +1,12 @@
 package MVC.model.SpecialMoves;
 
 
+import MVC.model.Board;
 import MVC.model.PieceLayoutFactory;
 import MVC.model.Pieces.King;
 import MVC.model.Pieces.Piece;
 import MVC.model.Pieces.Rook;
+import MVC.model.Pieces.MoveHandler;
 
 /**
  * Class that holds the logic for the castle-move.
@@ -23,11 +25,67 @@ public class Castle {
 
 
     public void switchKingRook(King king, Rook rook){
+    }
 
+    /**
+     * FIXA VARIABEL FÖR VILKEN ROOK SOM ÄR VILKEN OCH MATCHA MOT DEN
+     * @param pieces
+     * @param king
+     * @param rook
+     * @return
+     */
+
+
+    public boolean freeSpaceBetweenWKLWR(Piece[][] pieces, King king, Rook rook){
+        if(king.isPlayerOne() && rook.isPlayerOne()){
+            for (int row = 7; row < 8; row++) {
+                for (int col = 1; col < 4; col++) {
+                    if ((pieces[row][col]) == null){
+                        return true;
+                    }
+                }
+            }
+        }return false;
     }
 
 
-    public boolean freeSpaceBetweenWKLR(Piece[][] pieces, King king, Rook rook)
+    public boolean freeSpaceBetweenWKRWR(Piece[][] pieces, King king, Rook rook){
+        if(king.isPlayerOne() && rook.isPlayerOne()){
+            for (int row = 7; row < 8; row++) { // Coordinates need to be fixed
+                for (int col = 1; col < 4; col++) { // Coordinates need to be fixed
+                    if ((pieces[row][col]) == null){
+                        return true;
+                    }
+                }
+            }
+        }return false;
+    }
+
+    public boolean freeSpaceBetweenBKLBR(Piece[][] pieces, King king, Rook rook){
+        if(king.isPlayerOne() && rook.isPlayerOne()){
+            for (int row = 7; row < 8; row++) { // Coordinates need to be fixed
+                for (int col = 1; col < 4; col++) { // Coordinates need to be fixed
+                    if ((pieces[row][col]) == null){
+                        return true;
+                    }
+                }
+            }
+        }return false;
+    }
+
+    public boolean freeSpaceBetweenBKRBR(Piece[][] pieces, King king, Rook rook){
+        if(king.isPlayerOne() && rook.isPlayerOne()){
+            for (int row = 7; row < 8; row++) { // Coordinates need to be fixed
+                for (int col = 1; col < 4; col++) { // Coordinates need to be fixed
+                    if ((pieces[row][col]) == null){
+                        return true;
+                    }
+                }
+            }
+        }return false;
+    }
+
+
 
 
 
