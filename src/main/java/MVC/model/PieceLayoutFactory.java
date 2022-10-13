faf873e13bed27045a0274fb4510eccf50a6136a
Fruit7uice <36786942+Fruit7uice@users.noreply.github.com>
2022-10-13 15:58:50 +0200

    Factory uses x and y
    Travesing with rows and cols as this is more intuitive

--- src/main/java/MVC/model/PieceLayoutFactory.java
+++ src/main/java/MVC/model/PieceLayoutFactory.java
@@ -63,22 +63,22 @@ public class PieceLayoutFactory {
 
                 switch (s){
                     case "Rook":
-                        newPieces.add(PieceFactory.createRook(finalRow, col));
+                        newPieces.add(PieceFactory.createRook(col, finalRow));
                         break;
                     case "Knight":
-                        newPieces.add(PieceFactory.createKnight(finalRow, col));
+                        newPieces.add(PieceFactory.createKnight(col, finalRow));
                         break;
                     case "Bishop":
-                        newPieces.add(PieceFactory.createBishop(finalRow, col));
+                        newPieces.add(PieceFactory.createBishop(col, finalRow));
                         break;
                     case "Queen":
-                        newPieces.add(PieceFactory.createQueen(finalRow, col));
+                        newPieces.add(PieceFactory.createQueen(col, finalRow));
                         break;
                     case "King":
-                        newPieces.add(PieceFactory.createKing(finalRow, col));
+                        newPieces.add(PieceFactory.createKing(col, finalRow));
                         break;
                     case "Pawn":
-                        newPieces.add(PieceFactory.createPawn(finalRow, col));
+                        newPieces.add(PieceFactory.createPawn(col, finalRow));
                         break;
                     case ("null"):
                         newPieces.add(null);
