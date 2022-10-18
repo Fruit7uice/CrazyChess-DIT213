4eac99491c84f5963161ea3aad276529933a78d8
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-10-18 15:07:03 +0200

    Init of enPassant
    Started to implement enPassant

--- src/main/java/MVC/model/SpecialMoves/EnPassant.java
+++ src/main/java/MVC/model/SpecialMoves/EnPassant.java
@@ -1,8 +1,42 @@
 package MVC.model.SpecialMoves;
 
+import MVC.model.Pieces.Piece;
+
+import java.util.Objects;
+
 public class EnPassant {
-    private boolean isEnPassant = false;
 
-    
+    // Not completed as of right now.
 
-}
+
+    public boolean isWhiteEnPassant(Piece[][] pieceLayout, Piece pawn, int newX, int newY) {
+        if (pawn.yPos == 3 && pawn.isPlayerOne()) {
+            if (Objects.equals(pieceLayout[pawn.yPos][pawn.xPos - 1].getType(), "Pawn") && Objects.equals(pieceLayout[pawn.yPos][pawn.xPos - 1].getType(), !pawn.isPlayerOne()) ||
+                    Objects.equals(pieceLayout[pawn.yPos][pawn.xPos + 1].getType(), "Pawn") && Objects.equals(pieceLayout[pawn.yPos][pawn.xPos - 1].getType(), !pawn.isPlayerOne())) {
+            return true;
+            }
+        }return false;
+    }
+
+
+
+    public void whiteEnPassant(Piece[][] pieceLayout, Piece pawn, int newX, int newY){
+        if(isWhiteEnPassant(pieceLayout, pawn, newX, newY)){
+
+        }
+
+
+
+
+
+    }
+
+
+        public boolean isBlackEnPassant (Piece[][]pieceLayout, Piece pawn,int newX, int newY){
+
+            return false;
+        }
+
+
+    }
+
