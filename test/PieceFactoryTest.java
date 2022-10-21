c743c2dc6d0e5a1d441035e318f9d7a4ce41da95
Davhus <guseinda@student.gu.se>
2022-10-21 11:27:48 +0200

    Added tests for King movement in PieceTest and black pawn creation in PieceFactoryTest

--- test/PieceFactoryTest.java
+++ test/PieceFactoryTest.java
@@ -14,7 +14,49 @@ public class PieceFactoryTest {
 @Test
 public void createBlackPawn(){
     PieceFactory.isPlayerOne = false;
-    PieceFactory.createPawn(1,1);
+    Piece pawn = PieceFactory.createPawn(1,1);;
+    assertFalse(pawn.isPlayerOne());
+    assertEquals("Pawn", pawn.getType() );
+}
+@Test
+public void createWhitePawn(){
+    PieceFactory.isPlayerOne = true;
+    Piece pawn = PieceFactory.createPawn(1,1);
+    assertTrue(pawn.isPlayerOne());
     assertEquals("Pawn", PieceFactory.createPawn(1,1).getType() );
 }
+
+@Test
+public void createBlackRook(){
+    PieceFactory.isPlayerOne = false;
+    Piece rook = PieceFactory.createRook(1,1);
+    assertFalse(rook.isPlayerOne());
+    assertEquals("Rook", rook.getType());
+}
+
+@Test
+    public void createWhiteRook(){
+    PieceFactory.isPlayerOne = true;
+    Piece rook = PieceFactory.createRook(1,1);
+    assertTrue(rook.isPlayerOne());
+    assertEquals("Rook", rook.getType());
+}
+
+@Test
+    public void createBlackBishop(){
+    PieceFactory.isPlayerOne = false;
+    Piece bishop = PieceFactory.createBishop(1,1);
+    assertFalse(bishop.isPlayerOne());
+    assertEquals("Bishop", bishop.getType());
+}
+
+@Test
+    public void createWhiteBishop(){
+    PieceFactory.isPlayerOne = true;
+    Piece bishop = PieceFactory.createBishop(1,1);
+    assertTrue(bishop.isPlayerOne());
+    assertEquals("Bishop", bishop.getType());
+}
+
+
 }
