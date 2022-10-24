dfb86f3d3efe95fc75f01f868f60ef4dc5a2ad1d
Johannes Höher <89350186+johanneshoher@users.noreply.github.com>
2022-10-24 11:26:17 +0200

    Added JavaDoc to Observer
    JavaDoc for Observer and Observable.

--- src/main/java/MVC/model/Observable.java
+++ src/main/java/MVC/model/Observable.java
@@ -1,5 +1,12 @@
 package MVC.model;
 
+
+/**
+ * This interface enables the observer-pattern, if your class is supposed to be observed the class will implement
+ * this interface.
+ */
+
+
 public interface Observable {
 
     void addObserver(Observer o);
--- src/main/java/MVC/model/Observer.java
+++ src/main/java/MVC/model/Observer.java
@@ -1,7 +1,12 @@
 package MVC.model;
 
 import MVC.model.Pieces.Piece;
-import MVC.view.Tile;
+
+
+/**
+ * Interface for the Observer-pattern that only holds the method of update.
+ * The classes that implement this interface are for the ones that observe the observables.
+ */
 
 public interface Observer {
     void update(Piece[][] pieces);
